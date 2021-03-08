16b5f88f4640c899505d7cc18516cfdb7f8ddb75
JunHo <u_innovation@naver.com>
2021-03-08 22:10:15 +0900

    소켓 연결중...

--- ShareToDoList/app.js
+++ ShareToDoList/app.js
@@ -70,6 +70,6 @@ io.sockets.on('connection', function(socket) {
 })
 
 /* 서버를 8080 포트로 listen */
-server.listen(5000, function() {
+server.listen("1230", function() {
   console.log('서버 실행 중..')
 })
